14ca175deff2f8cff9c447719cb96d4d462ca81d
lluixhi <lluixhi@gmail.com>
2013-11-30 14:21:59 -0800

    Vanilla Branch.

--- startserver.bat
+++ startserver.bat
@@ -1,2 +1,2 @@
-java -Xmx3G -Xms2G -XX:MaxPermSize=128m -jar mcpc-plus-1.6.4-R2.1-forge953-B156.jar
+java -jar -Xmx3G -Xms2G -XX:MaxPermSize=128m minecraftforge-universal-1.6.4-9.11.1.953.jar nogui
 pause
