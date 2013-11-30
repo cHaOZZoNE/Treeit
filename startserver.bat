cfafbae20f4365aafc46b255fd4d23a9d143ce16
lluixhi <lluixhi@gmail.com>
2013-11-30 14:08:06 -0800

    MCPC+ Branch.

---
+++ startserver.bat
@@ -0,0 +1,2 @@
+java -Xmx3G -Xms2G -XX:MaxPermSize=128m -jar mcpc-plus-1.6.4-R2.1-forge953-B156.jar
+pause
---
+++ startserver.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+java -Xmx3G -Xms2G -XX:MaxPermSize=128m -jar mcpc-plus-1.6.4-R2.1-forge953-B156.jar
+
+# If you get memory errors, adjust the numbers.
