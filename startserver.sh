a4556fdbb103d78a745e6f032448901295308828
lluixhi <lluixhi@gmail.com>
2013-12-05 16:32:12 -0800

    3.2.3

--- startserver.sh
+++ startserver.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-java -Xmx3G -Xms2G -XX:MaxPermSize=128m -jar mcpc-plus-1.6.4-R2.1-forge953-B156.jar
+java -Xmx3G -Xms2G -XX:MaxPermSize=128m -jar mcpc-plus-1.6.4-R2.1-forge953-B158.jar
 
 # If you get memory errors, adjust the numbers.
