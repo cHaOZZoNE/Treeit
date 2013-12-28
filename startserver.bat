80f9b1b5ac954674712f1eb601c2c1d03830dcb3
lluixhi <lluixhi@gmail.com>
2013-12-28 14:17:02 -0800

    3.3.0

--- startserver.bat
+++ startserver.bat
@@ -1,2 +1,2 @@
-java -jar -Xmx3G -Xms2G -XX:MaxPermSize=128m minecraftforge-universal-1.6.4-9.11.1.953.jar nogui
+java -jar -Xmx3G -Xms2G -XX:MaxPermSize=128m minecraftforge-universal-1.6.4-9.11.1.965-v164-pregradle.jar nogui
 pause
